152c9045229b1c97c15a5ddf724c1a1fa3f27ba7
lily healey <lily@lilyhealey.co.uk>
2015-12-08 13:37:51 -0500

    display gifs, need to fix menu

--- index.php
+++ index.php
@@ -89,37 +89,17 @@ else
 			$m_file = "MEDIA/";
 			$m_file.= str_pad($myrow["mediaId"], 5, "0", STR_PAD_LEFT);
 			$m_file.= ".".$myrow["type"];
-			$m_caption = strip_tags($myrow["caption"]);
+			$m_caption = $myrow["caption"];
 	 		$m_style = "width: 100%;";
 			$image_files[$i] = $m_file;
-			$captions[$i] = $m_caption;
-		
-			// build random styles
-			$randomPadding = rand(0, 15);
-			$randomPadding *= 10;
-			$randomWidth = rand(4, 7);
-			$randomWidth *= 10;
-			$randomFloat = (rand(0, 1) == 0) ? 'left' : 'right';
-			$icStyle = 'width:'.$randomWidth.'%; ';
-			$icStyle .= 'float:'.$randomFloat.'; ';
-			$icStyle .= 'padding-top:'.$randomPadding.'px; ';
-			$icStyle .= 'margin: 40px;'; 
 
 			$images[$i] .= "<div ";
 			$images[$i] .= "id='image".$i."' ";
 			$images[$i] .= "style='".$icStyle."' ";
 			$images[$i] .= ">";
 		
-			$images[$i].= "<a ";
-			$images[$i].= "href='$u?id=".$myrow['objectsId']."'";
-			$images[$i].= "class='img-container' ";
-			$images[$i].= ">";
 			$images[$i].= displayMedia($m_file, $m_caption, $m_style);
-			$images[$i].= "</a>";
 		
-			$images[$i].= "<div class='caption'>";
-			$images[$i].= $myrow['name1'];
-			$images[$i].= "</div>";
 			$images[$i].= "</div>";
 			$id_last = $myrow['objectsId'];
 		}
@@ -135,6 +115,8 @@ else
 			$b = trim($body[$i]);
 			$b = Markdown::defaultTransform($b);
 			echo $b;
+			if($i == 0)
+				echo $images[0];
 			?></div>
 			<div class="column-container visible"><?
 		}
