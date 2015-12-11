2724afb6a0d44bfd9b4bc52194247345bdf2e00d
lily healey <lily@lilyhealey.co.uk>
2015-12-11 11:24:35 -0500

    trim css

---
+++ views/clock.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="STATIC/JS/clock.js"></script>
+<canvas id="clock-canvas" class="v-centre"></canvas>
+<script>init_clock("clock-canvas");</script>
+<script type="text/javascript" src="STATIC/JS/global.js"></script>
