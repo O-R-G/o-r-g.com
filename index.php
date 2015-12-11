ec73926492cd433a6741b790c35ee93e538fee83
lily healey <lily@lilyhealey.co.uk>
2015-12-11 11:26:48 -0500

    update css

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 require_once("inc/head.php");
-require_once("views/cover.php");
+// only show the clock on the homepage
+if($uu->id)
+	require_once("views/object.php");
+else
+	require_once("views/clock.php");
 require_once("inc/foot.php");
 ?>
