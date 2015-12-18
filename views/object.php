23b0623621a647f038eb740cd97df4aa19437b15
lily healey <lily@lilyhealey.co.uk>
2015-12-18 16:24:48 -0500

    add breadcrumb menu

--- views/object.php
+++ views/object.php
@@ -21,7 +21,25 @@ $oarr = $oo->get($uu->id);
 $body = $oarr["body"];
 $b_arr = process_body($body);
 $marr = $oo->media($uu->id);
-?><section id="body" class="visible"><?
+
+$ancestors = $oo->ancestors($uu->id);
+$a_url = "";
+?><section id="body" class="visible">
+	<div id="breadcrumbs"><?
+		for($i = 0; $i < count($ancestors); $i++)
+		{
+			$a = $oo->get($ancestors[$i]);
+			$a_url.= $a["url"];
+		?><ul class="nav-level">
+			<li><a href="<? echo $host.$a_url; ?>"><? echo $a["name1"]; ?></a></li>
+		</ul><?
+			$a_url.= "/";
+		}
+		?><ul class="nav-level"><?
+			$a_url.= $item["url"];
+			?><a href="<? echo $host.$a_url; ?>"><? echo $name; ?></a>
+		</ul>
+	</div><?
 for($i = 0; $i < count($b_arr); $i++)
 {
 	if($i % 2 == 0)
