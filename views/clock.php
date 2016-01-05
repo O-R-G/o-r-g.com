af53c62fe7860b073019f7641847bc2d4817aed6
lily healey <lily@lilyhealey.co.uk>
2016-01-05 11:07:07 -0500

    make clock small on download page

--- views/clock.php
+++ views/clock.php
@@ -1,5 +1,5 @@
 <? ?><script type="text/javascript" src="<? echo $host; ?>static/js/clock.js"></script><?
-if(!$uu->id)
+if(!$uu->id && $uri[1] != "thx")
 {
 ?>
 <div id="canvas-container" class="v-centre">
@@ -16,10 +16,10 @@ else
 <script>init_clock("clock-canvas", "lower-right");</script><?
 }
 ?><script type="text/javascript" src="<? echo $host; ?>static/js/global.js"></script><?
-if($uu->id)
+if($uu->id || $uri[1] == "thx")
 {
 ?><script>showHide();</script><?
-	if(empty($oo->children_ids($uu->id)))
+	if(empty($oo->children_ids($uu->id)) || $uri[1] == "thx")
 	{
 ?><script>showHide();</script><?
 	}
