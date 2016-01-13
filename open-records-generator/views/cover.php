f0a12d56b5a597cae6f6062dd11ebfca2a3ac7d0
lily healey <lily@lilyhealey.co.uk>
2016-01-13 11:43:55 -0500

    update open-records-generator styling (cover, edit)

--- open-records-generator/views/cover.php
+++ open-records-generator/views/cover.php
@@ -3,7 +3,7 @@
 		<div class="parent-container">
 			<div class="parent">Welcome to the OPEN RECORDS GENERATOR.</div>
 			<div class="parent"><?php echo date("d M Y H:i:s T"); ?></div>
-			<div class="parent">
+			<div class="button-container">
 				<a href="<? echo $admin_path; ?>browse">Enter database...</a>
 			</div>
 		</div>
