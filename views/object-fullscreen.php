71c126295f392bc636d2f7f4842b7179fe46bd97
David Reinfurt <reinfurt@o-r-g.com>
2016-01-15 11:08:11 -0500

    add *now*

--- views/object-fullscreen.php
+++ views/object-fullscreen.php
@@ -25,7 +25,7 @@ $marr = $oo->media($uu->id);
 			<li><?
 				if(!$uu->id)
 				{
-				?> O-R-G is a small software company.<?
+				?> O-R-G is <a href="now">*now*</a> a small software company.<?
 				}
 				else
 				{
