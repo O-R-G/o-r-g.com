62ea1165647a14ec9181e1c8326a6dfdebbb71ef
david reinfurt <reinfurt@ussava.pair.com>
2016-01-28 11:12:47 -0500

    add static/js/analytics.js

--- views/head.php
+++ views/head.php
@@ -56,14 +56,7 @@ if($uu->id)
 		<link rel="stylesheet" href="<? echo $host; ?>static/css/global.css">
 		<link rel="stylesheet" href="<? echo $host; ?>static/css/sf-text.css">
 		<link rel="apple-touch-icon" href="<? echo $host; ?>media/png/touchicon.png" />
-		<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-72957405-2', 'auto');
-  ga('send', 'pageview');
-		</script>
+		<script src="static/js/analytics.js"></script>
 	</head>
 	<body>
 		<div id="page"><?
