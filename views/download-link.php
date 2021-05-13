59756e6bc64a29537741dd0c06e107a1c7883e91
Wei-Hao Wang <wwang03@risd.edu>
2021-05-13 22:29:58 +0800

    update download-link.php

--- views/download-link.php
+++ views/download-link.php
@@ -1,4 +1,70 @@
 <?
+/*
+    PDT php example: 
+    https://github.com/paypal/pdt-code-samples/blob/master/paypal_pdt.php
+*/
+
+
+$paypal_id = end($oo->urls_to_ids(array('shop', 'paypal')));
+$paypal_items = $oo->get($paypal_id);
+$bracket_pattern = '#\[(.*)\]#is';
+preg_match_all($bracket_pattern, $paypal_items['deck'], $temp);
+$auth_token = $temp[1][0];
+
+// $pp_hostname = "www.paypal.com";
+$pp_hostname = "www.sandbox.paypal.com";
+$req = 'cmd=_notify-synch';
+$tx_token = $_GET['tx'];
+$req .= "&tx=$tx_token&at=$auth_token";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://$pp_hostname/cgi-bin/webscr");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+//set cacert.pem verisign certificate path in curl using 'CURLOPT_CAINFO' field here,
+//if your server does not bundled with default verisign certificates.
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Host: $pp_hostname"));
+$res = curl_exec($ch);
+curl_close($ch);
+
+if(!$res){
+    //HTTP ERROR
+}else{
+     // parse the data
+    $lines = explode("\n", trim($res));
+    $keyarray = array();
+    if (strcmp ($lines[0], "SUCCESS") == 0) {
+        for ($i = 1; $i < count($lines); $i++) {
+            $temp = explode("=", $lines[$i],2);
+            $keyarray[urldecode($temp[0])] = urldecode($temp[1]);
+        }
+    var_dump($keyarray);
+    // check the payment_status is Completed
+    // check that txn_id has not been previously processed
+    // check that receiver_email is your Primary PayPal email
+    // check that payment_amount/payment_currency are correct
+    // process payment
+    // $firstname = $keyarray['first_name'];
+    // $lastname = $keyarray['last_name'];
+    // $itemname = $keyarray['item_name'];
+    // $amount = $keyarray['payment_gross'];
+     
+    // echo ("<p><h3>Thank you for your purchase!</h3></p>");
+     
+    // echo ("<b>Payment Details</b><br>\n");
+    // echo ("<li>Name: $firstname $lastname</li>\n");
+    // echo ("<li>Item: $itemname</li>\n");
+    // echo ("<li>Amount: $amount</li>\n");
+    // echo ("");
+    }
+    else if (strcmp ($lines[0], "FAIL") == 0) {
+        // log for manual investigation
+    }
+}
+
+/*
 var_dump($_POST);
 $num_cart_items = intval($_POST['num_cart_items']);
 for ($i = 1; $i <= $num_cart_items; $i++ ) {
@@ -47,5 +113,6 @@ foreach($downloadLink as $key => $link){
     <div><a href="<?= $link; ?>"><?= $item_name_clean[$key]; ?></a></div>
     <?
 }
+*/
 ?>
 
